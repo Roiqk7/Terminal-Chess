62531014dfa0b4ca983bfa7a8b64b56c836d50c9
Roiqk7 <petrsevecek@icloud.com>
2024-06-21 19:25:22 +0200

    Added

---
+++ include/window.hpp
@@ -0,0 +1,7 @@
+/*
+Date: 21/06/2024
+
+Description: This file defines window related functions.
+
+Notes: x
+*/
---
+++ src/window.cpp
@@ -0,0 +1,7 @@
+/*
+Date: 21/06/2024
+
+Description: This file implements window related functions.
+
+Notes: x
+*/
