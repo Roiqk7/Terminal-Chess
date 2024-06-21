be7d1d7e3f2d088b16578582eae1afe069e57c81
Roiqk7 <petrsevecek@icloud.com>
2024-06-21 08:16:26 +0200

    Added entry point

---
+++ src/terminalChess.cpp
@@ -0,0 +1,15 @@
+/*
+Date: 21/06/2024
+
+Description: This is the entry point of the program. It contains the main function.
+
+Notes: x
+*/
+
+#include "../include/flowControl.hpp"
+
+int main()
+{
+        Chess::FlowControl::mainApplicationLoop();
+        return 0;
+}
