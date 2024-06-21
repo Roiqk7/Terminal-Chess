e0380faaeffb19926c796996a458f890dbb11b1d
Roiqk7 <petrsevecek@icloud.com>
2024-06-21 07:42:02 +0200

    Cosmetics

--- include/gui.hpp
+++ include/gui.hpp
@@ -22,10 +22,14 @@ namespace Chess
                 {
                         // TODO: Add graphics constants
                 }
-
+        // Application
+                void displayMainMenu();                                         // Display the main menu
+        // Gameplay
                 void displayBoard(const Globals::FEN& fen);                     // Display the chess board
                 void displayTile(const Globals::Coordinate& coordinate,
                         const Globals::FEN& fenChar);                           // Display a tile with(out) a piece on the board
+                void displayGameOverMenu(const Globals::Color& winner);         // Display the game over menu
+        // Messages
                 void displayMessage(const std::string& message);                // Display a message
         }
 }
