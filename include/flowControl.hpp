5301f40309bfd819c53c65d284b6e2d8a2643737
Roiqk7 <petrsevecek@icloud.com>
2024-06-21 07:18:56 +0200

    Added declarations

--- include/flowControl.hpp
+++ include/flowControl.hpp
@@ -8,4 +8,14 @@ Notes: x
 #ifndef FLOW_CONTROL_HPP
 #define FLOW_CONTROL_HPP
 
+namespace Chess
+{
+        namespace FlowControl
+        {
+                void mainApplicationLoop();                                     // Makes sure the application proceeds as expected
+                void startApplication();                                        // Start the application
+                void endApplication();                                          // End the application
+        }
+}
+
 #endif // !FLOW_CONTROL_HPP
