22b635d4861c67af45a4ef4d97319a0a00d41286
Roiqk7 <petrsevecek@icloud.com>
2024-06-22 10:37:40 +0200

    Implemented

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -29,13 +29,32 @@ namespace Chess
                 }
 
                 /*
-                Handle the width of the scene.
+                Handle the width of the scene. Center the graphics of each element.
 
                 @param scene: The scene to be handled
                 */
                 void handleSceneWidth(Scene& scene)
                 {
-                        // TODO: Implement handleSceneWidth
+                        // Center the graphics of each element
+                        for (auto& element : scene.elements)
+                        {
+                                std::vector<std::string> centeredGraphics;
+
+                                // Each line must be GUI_WIDTH characters long
+                                for (const auto& line : element.graphics)
+                                {
+                                        size_t padding = (Globals::GUI_WIDTH
+                                                - line.length()) / 2;
+                                        std::string centeredLine(padding, ' '); // Create left padding
+                                        centeredLine += line;                   // Add the original line
+                                        centeredLine += std::string(
+                                                Globals::GUI_WIDTH
+                                                - centeredLine.length(), ' ');  // Add right padding to fill up to GUI_WIDTH
+                                        centeredGraphics.push_back(
+                                                centeredLine);
+                                }
+                                element.graphics = centeredGraphics;            // Update the element's graphics with the centered version
+                        }
                 }
 
                 /*
@@ -45,7 +64,51 @@ namespace Chess
                 */
                 void handleSceneHeight(Scene& scene)
                 {
-                        // TODO: Implement handleSceneHeight
+                        // First we calculate the dimensions of the scene
+                        scene.calculateDimensions();
+
+                        // Calculate the number of empty lines to add to fit the GUI_HEIGHT
+                        size_t emptyLines = Globals::GUI_HEIGHT - scene.height;
+
+                        // No calculate the number of places where we can add empty lines
+                        size_t emptyPlaces = scene.elements.size() - 1;
+
+                        // Calculate the number of empty lines to add to each place
+                        size_t emptyLinesPerPlace = emptyLines / emptyPlaces;
+
+                        // Calculate the number of remaining empty lines (those will be added to the first places equally)
+                        size_t remainingEmptyLines = emptyLines % emptyPlaces;
+
+                        // Calculate the number of remaining empty lines per place
+                        size_t remainingEmptyLinesPerPlace = remainingEmptyLines / emptyPlaces;
+
+                        // Add the empty lines in between the elements
+                        std::vector<std::string> formattedGraphics;
+
+                        // First add the element
+                        for (auto element : scene.elements)
+                        {
+                                for (auto line : element.graphics)
+                                {
+                                        formattedGraphics.push_back(line);
+                                }
+
+                                // Add empty lines
+                                for (size_t i = 0; i < emptyLinesPerPlace; i++)
+                                {
+                                        formattedGraphics.push_back(" ");
+                                }
+
+                                // Add remaining empty lines
+                                if (remainingEmptyLines > 0)
+                                {
+                                        for (size_t i = 0; i < remainingEmptyLinesPerPlace; i++)
+                                        {
+                                                formattedGraphics.push_back(" ");
+                                                remainingEmptyLines--;
+                                        }
+                                }
+                        }
                 }
         }
 }
