8ac4242a241650dacc55a94b8b6bd8031367ebbc
Roiqk7 <petrsevecek@icloud.com>
2024-06-22 09:31:46 +0200

    Basic template

--- include/formatter.hpp
+++ include/formatter.hpp
@@ -5,3 +5,17 @@ Description: This file defines formatter functions.
 
 Notes: x
 */
+
+#include <string>
+#include <vector>
+#include "element.hpp"
+#include "globals.hpp"
+#include "scene.hpp"
+
+namespace Chess
+{
+        namespace GUI
+        {
+
+        }
+}
