de30317248e076bf96e7685ced2ee45acba0aa84
Roiqk7 <petrsevecek@icloud.com>
2024-06-22 08:22:11 +0200

    Rewritten

--- include/scene.hpp
+++ include/scene.hpp
@@ -8,34 +8,21 @@ Notes: x
 
 #include <string>
 #include <vector>
+#include "element.hpp"
 #include "globals.hpp"
 
 namespace Chess
 {
         namespace GUI
         {
-        // Element
-                struct Element
-                {
-                public: // Variables
-                        std::string name;                                       // Name of the element
-                        std::string filePath;                                   // File path of the underlying asset
-                        std::vector<std::string> graphics;                      // Graphics to be displayed
-                        size_t width;                                           // Width of the element
-                        size_t height;                                          // Height of the element
-                public: // Constructors
-                        Element(const std::string& filePath);                   // Constructor with file path
-                        Element(const std::vector<std::string>& graphics);      // Constructor with graphics
-                public: // Operators
-                        bool operator==(const Element& element) const;          // Equality operator
-                };
         // Scene
-        // ? What about input elements? You will may need to remake the entire GUI system to make it work better???
-        // ! Do not worry... it will likely take few hours and save some in the end...
                 class Scene
                 {
                 public: // Variables
-                        std::vector<Element> elements;                          // Elements of the scene (sorted in order of display)
+                        std::vector<std::string> graphics;                      // Graphics to be displayed
+                        size_t width;                                           // Width of the scene
+                        size_t height;                                          // Height of the scene
+                        bool raw;                                               // Whether the scene is raw (not ready for display) or not
                 public: // Methods
                 // Constructors
                         Scene();                                                // Default constructor
@@ -46,6 +33,8 @@ namespace Chess
                         void removeElement(const size_t& index);                // Remove an element from the scene
                         void updateElement(
                                 const std::vector<std::string>& graphics);      // Update the graphics of an element
+                private: // Variables
+                        std::vector<Element> elements;                          // Elements in the scene
                 };
         }
 }
