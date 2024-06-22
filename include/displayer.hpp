6a06f0fb18dfcbe7cfa54805ef817e0b81e56594
Roiqk7 <petrsevecek@icloud.com>
2024-06-22 08:39:23 +0200

    Created

---
+++ include/displayer.hpp
@@ -0,0 +1,22 @@
+/*
+Date: 22/06/2024
+
+Description: This file implements displayer (renderer) related classes and functions.
+
+Notes: x
+*/
+
+#include <string>
+#include <vector>
+#include "globals.hpp"
+
+namespace Chess
+{
+        namespace Displayer
+        {
+                class Displayer
+                {
+                public: // Methods
+                };
+        }
+}
