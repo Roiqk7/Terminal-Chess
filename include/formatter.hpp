748eb6592168d9f5d78d92b591e7ed63dbb06047
Roiqk7 <petrsevecek@icloud.com>
2024-06-22 10:09:45 +0200

    Reordered definitions

--- include/formatter.hpp
+++ include/formatter.hpp
@@ -8,8 +8,6 @@ Notes: x
 
 #include <string>
 #include <vector>
-#include "element.hpp"
-#include "globals.hpp"
 #include "scene.hpp"
 
 namespace Chess
@@ -17,10 +15,8 @@ namespace Chess
         namespace GUI
         {
                 void formatScene(Scene& scene);                                         // Format the scene for display
-                void handleSceneHeight(Scene& scene);                                   // Handle the height of the scene
                 void handleSceneWidth(Scene& scene);                                    // Handle the width of the scene
-                // Note: Maybe scenes should just be elements and the graphics will be given here
-                // That way there is no problem with 'preformatted' graphics where the width will then be wrong
-                // ? Check if the above is relevant and just make sure graphics is given here.
+                void handleSceneHeight(Scene& scene);                                   // Handle the height of the scene
+
         }
 }
