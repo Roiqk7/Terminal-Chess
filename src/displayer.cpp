d7899e441a7aeee7b7c6d909d91445c2c363a68a
Roiqk7 <petrsevecek@icloud.com>
2024-06-22 11:03:58 +0200

    Added log

--- src/displayer.cpp
+++ src/displayer.cpp
@@ -9,6 +9,8 @@ Notes: x
 #include <iostream>
 #include <string>
 #include <vector>
+#include "../include/displayer.hpp"
+#include "../include/globals.hpp"
 #include "../include/scene.hpp"
 
 namespace Chess
@@ -20,22 +22,12 @@ namespace Chess
 
                 @param scene: The scene to be displayed
                 */
-                void display(Scene& scene)
-                {
-                        // Display the graphics line by line
-                        for (const auto& line : scene.graphics)
-                        {
-                                std::cout << line << '\n';
-                        }
-                }
-
-                /*
-                Display the scene.
-
-                @param scene: The scene to be displayed
-                */
                 void display(const Scene& scene)
                 {
+                        // Log the display
+                        LOG_TRACE("Displaying {} scene {} with width {} and height {}",
+                                scene.raw ? "raw" : "formatted", scene.name, scene.width, scene.height);
+
                         // Display the graphics line by line
                         for (const auto& line : scene.graphics)
                         {
