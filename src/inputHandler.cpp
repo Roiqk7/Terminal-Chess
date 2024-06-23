07453e59f7ac641c8336f1f5263b6df51712f32f
Roiqk7 <petrsevecek@icloud.com>
2024-06-23 09:20:39 +0200

    Added a rather long function to handle valid user input...

--- src/inputHandler.cpp
+++ src/inputHandler.cpp
@@ -6,8 +6,11 @@ Description: This file implements functions which handle user input.
 Notes: x
 */
 
+#include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "../include/displayer.hpp"
 #include "../include/inputHandler.hpp"
 #include "../include/globals.hpp"
@@ -46,6 +49,110 @@ namespace Chess
                         LOG_TRACE("User input: " + input);
 
                         return input;
+                }
+
+                /*
+                Validate user input.
+
+                @param input: The user's input
+                @param gameState: The current game state
+
+                @return: True if the input is valid, false otherwise
+                */
+                bool validateUserInput(const std::string& input,
+                        Globals::GameState gameState)
+                {
+                        // Check empty input
+                        if (input.empty())
+                        {
+                                return false;
+                        }
+
+                        // Lowercase the input
+                        std::string lowercaseInput;
+                        lowercaseInput.resize(input.size());
+                        std::transform(input.begin(), input.end(), lowercaseInput.begin(),
+                                [](unsigned char c){ return std::tolower(c); });
+
+                        // Vector of valid inputs
+                        std::vector<std::string> validInputs;
+
+                        // Check valid input based on the game state
+                        // Find out more about valid inputs in docs/gameStates.md
+                        switch (gameState)
+                        {
+                                case Globals::GameState::MainMenu:
+                                        validInputs = {"p", "play", "h", "help", "q", "quit", "c", "credits"};
+                                        if (std::find(validInputs.begin(), validInputs.end(), lowercaseInput) != validInputs.end())
+                                        {
+                                                return true;
+                                        }
+                                        break;
+                                case Globals::GameState::Credits:
+                                        validInputs = {"q", "quit"};
+                                        if (std::find(validInputs.begin(), validInputs.end(), lowercaseInput) != validInputs.end())
+                                        {
+                                                return true;
+                                        }
+                                        break;
+                                case Globals::GameState::HelpMenu:
+                                        validInputs = {"q", "quit"};
+                                        if (std::find(validInputs.begin(), validInputs.end(), lowercaseInput) != validInputs.end())
+                                        {
+                                                return true;
+                                        }
+                                        break;
+                                case Globals::GameState::GameModeMenu:
+                                        validInputs = {"s", "single", "singleplayer", "single-player", "m", "multi", "multiplayer", "q", "quit"};
+                                        if (std::find(validInputs.begin(), validInputs.end(), lowercaseInput) != validInputs.end())
+                                        {
+                                                return true;
+                                        }
+                                        break;
+                                case Globals::GameState::DifficultyMenu:
+                                        validInputs = {"e", "easy", "m", "medium", "h", "hard", "q", "quit"};
+                                        if (std::find(validInputs.begin(), validInputs.end(), lowercaseInput) != validInputs.end())
+                                        {
+                                                return true;
+                                        }
+                                        break;
+                                case Globals::GameState::GamePlay:
+                                        // TODO: Add valid inputs for game play
+                                        break;
+                                case Globals::GameState::PauseMenu:
+                                        validInputs = {"r", "resume", "s", "save", "l", "load", "q", "quit"};
+                                        if (std::find(validInputs.begin(), validInputs.end(), lowercaseInput) != validInputs.end())
+                                        {
+                                                return true;
+                                        }
+                                        break;
+                                case Globals::GameState::SaveGame:
+                                        validInputs = {"1", "2", "3", "q", "quit"};
+                                        if (std::find(validInputs.begin(), validInputs.end(), lowercaseInput) != validInputs.end())
+                                        {
+                                                return true;
+                                        }
+                                        break;
+                                case Globals::GameState::LoadGame:
+                                        validInputs = {"1", "2", "3", "q", "quit"};
+                                        if (std::find(validInputs.begin(), validInputs.end(), lowercaseInput) != validInputs.end())
+                                        {
+                                                return true;
+                                        }
+                                        break;
+                                case Globals::GameState::GameOver:
+                                        validInputs = {"r", "restart", "q", "quit"};
+                                        if (std::find(validInputs.begin(), validInputs.end(), lowercaseInput) != validInputs.end())
+                                        {
+                                                return true;
+                                        }
+                                        break;
+                                default:
+                                        LOG_ERROR("Invalid game state.");
+                                        return false;
+                        }
+
+                        return false;
                 }
         }
 }
