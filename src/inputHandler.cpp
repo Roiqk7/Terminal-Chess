876ca87af48d0a570a668e9a8228f329a8ef8b80
Roiqk7 <petrsevecek@icloud.com>
2024-06-23 08:35:10 +0200

    Implemented getUserInput()

--- src/inputHandler.cpp
+++ src/inputHandler.cpp
@@ -5,3 +5,44 @@ Description: This file implements functions which handle user input.
 
 Notes: x
 */
+
+#include <iostream>
+#include <string>
+#include "../include/displayer.hpp"
+#include "../include/inputHandler.hpp"
+#include "../include/globals.hpp"
+#include "../include/gui.hpp"
+#include "../include/tool.hpp"
+
+namespace Chess
+{
+        namespace InputHandler
+        {
+                /*
+                Get user input from the console.
+
+                @param prompt: The prompt to display to the user
+
+                @return: The user's input
+                */
+                std::string getUserInput(const std::string& prompt)
+                {
+                        std::string input;
+
+                        // Center the prompt
+                        std::string centeredPrompt = Tool::centerString(
+                                prompt, Globals::GUI_WIDTH, false);
+
+                        // Display the prompt
+                        GUI::displayString(centeredPrompt);
+
+                        // Get the input
+                        std::getline(std::cin, input);
+
+                        // Log the input
+                        LOG_TRACE("User input: " + input);
+
+                        return input;
+                }
+        }
+}
