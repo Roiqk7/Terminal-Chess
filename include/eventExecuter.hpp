94d907d0c4e6dcede3f778740f83e69b1dbb26af
Roiqk7 <petrsevecek@icloud.com>
2024-06-24 07:28:31 +0200

    Added

---
+++ include/eventExecuter.hpp
@@ -0,0 +1,27 @@
+/*
+Date: 24/06/2024
+
+Description: This file contains class definition for the EventExecuter class.
+
+Notes: x
+*/
+
+#ifndef EVENT_EXECUTER_HPP
+#define EVENT_EXECUTER_HPP
+
+#include <memory>
+#include "event.hpp"
+#include "globals.hpp"
+
+namespace Chess
+{
+        namespace EventSystem
+        {
+                class EventExecuter
+                {
+                // TODO: Implement the EventExecuter class
+                };
+        }
+}
+
+#endif // !EVENT_EXECUTER_HPP
