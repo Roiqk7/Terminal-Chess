b9f208fb2c13e022cab8c17433f860ffff070ba5
Roiqk7 <petrsevecek@icloud.com>
2024-06-27 12:26:59 +0200

    Added

---
+++ include/chessLibrary.hpp
@@ -0,0 +1,20 @@
+/*
+Date: 22/06/2024
+
+Description: This file defines the interface with the chess library.
+
+Notes: x
+*/
+
+#ifndef CHESS_LIBRARY_HPP
+#define CHESS_LIBRARY_HPP
+
+namespace Chess
+{
+        namespace IChessLibrary
+        {
+
+        }
+}
+
+#endif // !CHESS_LIBRARY_HPP
