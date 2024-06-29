61c16b26bf3cbeaa0f408a193094b94c83fcca10
Roiqk7 <petrsevecek@icloud.com>
2024-06-29 09:45:16 +0200

    Added basics

--- include/gameHandler.hpp
+++ include/gameHandler.hpp
@@ -9,15 +9,16 @@ Notes: By game we mean the entire application, not just the chess game.
 #ifndef GAME_HANDLER_HPP
 #define GAME_HANDLER_HPP
 
+#include "../lib/chess-library/include/chess.hpp"
+
 namespace Chess
 {
         namespace GameHandler
         {
-                void mainGameLoop();                                            // Makes sure the game proceeds as expected
-                void startGame();                                               // Create a new game from scratch
-                void endGame();                                                 // Finish the current game
-                void saveGame();                                                // Save the current game
-                void loadGame();                                                // Load a saved game
+                void mainGameLoop(chess::Board& board);                         // Makes sure the game proceeds as expected
+                chess::Board startGame(const std::string& fen);                 // Create a new game
+                void saveGame(const std::string& fen, const int saveSlot);      // Save the current game
+                void loadGame(const int saveSlot);                              // Load a saved game
         }
 }
 
--- src/gameHandler.cpp
+++ src/gameHandler.cpp
@@ -6,6 +6,7 @@ Description: This file implements functions which handle individual chess game.
 Notes: By game we mean the entire application, not just the chess game.
 */
 
+#include "../include/chessLibrary.hpp"
 #include "../include/gameHandler.hpp"
 
 namespace Chess
@@ -13,21 +14,48 @@ namespace Chess
         namespace GameHandler
         {
                 /*
-                This function controls the entire game.
+                Makes sure the game proceeds as expected
+
+                @param board: The chess board.
                 */
-                void mainGameLoop()
+                void mainGameLoop(chess::Board& board)
                 {
-                        // Placeholder
+                        // TODO: Implement the main game loop
                 }
 
-                void startGame()
+                /*
+                Create a new game
+
+                @param fen: The FEN string representing the board state. If empty, the default starting position is used.
+
+                @return: The chess board.
+                */
+                chess::Board startGame(const std::string& fen)
                 {
-                        // Placeholder
+                        chess::Board board;
+                        Chess::IChessLibrary::setupBoard(board, fen);
+                        return board;
                 }
 
-                void endGame()
+                /*
+                Save the current game
+
+                @param fen: The FEN string representing the board state.
+                @param saveSlot: The slot in which to save the game.
+                */
+                void saveGame(const std::string& fen, const int saveSlot)
                 {
-                        // Placeholder
+                        // TODO: Implement saving the game
+                }
+
+                /*
+                Load a saved game
+
+                @param saveSlot: The slot from which to load the game.
+                */
+                void loadGame(const int saveSlot)
+                {
+                        // TODO: Implement loading a saved game
                 }
         }
 }
